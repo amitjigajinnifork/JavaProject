733ae5e7dbb21db302e48e0c5c49cdd90963406e
amit_fork <amitjigajinnisd@gmail.com>
2016-04-19 14:33:22 -0500

    adding and commiting modified files

---
+++ MyFirstJavaProject/src/com/example/util/DateUtil.java
@@ -0,0 +1,7 @@
+package com.example.util;
+
+import java.util.Date;
+
+public class DateUtil {
+Date local_date = new Date();
+}
---
+++ MyFirstJavaProject/src/com/example/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.example.util;
+
+public class StringUtil {
+	public String convertLower(String str){
+		str= "testing";
+		return str.toLowerCase();
+	}
+	
+	public String convertUpper(String str){
+		return str.toUpperCase();
+	}
+	
+	
+}
